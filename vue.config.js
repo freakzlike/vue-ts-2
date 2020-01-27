5000a96dfd916d10169113ab1aaa3d3d5fa503b4
JSTE <freakzlike@gmail.com>
2020-01-27 20:39:37 +0100

    Add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,22 @@
-const webpack = require('webpack')
-
-// Split chunks only on production, cause it will break in tests
-const optimization = process.env.NODE_ENV === 'production' ? {
-  splitChunks: {
-    minSize: 10000,
-    maxSize: 250000
-  }
-} : undefined
-
 module.exports = {
   lintOnSave: false,
   productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
   ],
-
   configureWebpack: {
     plugins: [
-      // Ignore all locale files of moment.js
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/locale$/,
-        contextRegExp: /moment$/
-      })
-    ],
-    optimization
+      {
+        options: {
+          resourceRegExp: {},
+          contextRegExp: {}
+        }
+      }
+    ]
   },
-
   devServer: {
     watchOptions: {
-      ignored: /node_modules/,
+      ignored: {},
       aggregateTimeout: 600,
       poll: 2000
     },
