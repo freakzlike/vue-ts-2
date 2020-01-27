910a5595269b053d4331eb6ddc46ee07f4124838
JSTE <freakzlike@gmail.com>
2020-01-27 21:02:41 +0100

    Prepare files for prototyping

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,13 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/script-indent': ['error', 2, {
+      'baseIndent': 1,
+      'switchCase': 1,
+      'ignores': []
+    }],
+    'object-curly-spacing': 0
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
@@ -23,6 +29,12 @@ module.exports = {
       ],
       env: {
         jest: true
+      }
+    },
+    {
+      files: ['*.vue'],
+      rules: {
+        'indent': 'off'
       }
     }
   ]
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,34 @@
+const webpack = require('webpack')
+
+// Split chunks only on production, cause it will break chai in tests
+const optimization = process.env.NODE_ENV === 'production' ? {
+  splitChunks: {
+    minSize: 10000,
+    maxSize: 250000
+  }
+} : undefined
+
 module.exports = {
   lintOnSave: false,
   productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
   ],
+
   configureWebpack: {
     plugins: [
-      {
-        options: {
-          resourceRegExp: {},
-          contextRegExp: {}
-        }
-      }
-    ]
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/
+      })
+    ],
+    optimization
   },
+
   devServer: {
     watchOptions: {
-      ignored: {},
+      ignored: /node_modules/,
       aggregateTimeout: 600,
       poll: 2000
     },
