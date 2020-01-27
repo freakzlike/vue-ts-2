7fa109c407bab0d070c71a302d820591ff4d4ae0
JSTE <freakzlike@gmail.com>
2020-01-27 20:19:09 +0100

    Clean up and add default vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,39 @@
+const webpack = require('webpack')
+
+// Split chunks only on production, cause it will break in tests
+const optimization = process.env.NODE_ENV === 'production' ? {
+  splitChunks: {
+    minSize: 10000,
+    maxSize: 250000
+  }
+} : undefined
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  productionSourceMap: false,
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  configureWebpack: {
+    plugins: [
+      // Ignore all locale files of moment.js
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/
+      })
+    ],
+    optimization
+  },
+
+  devServer: {
+    watchOptions: {
+      ignored: /node_modules/,
+      aggregateTimeout: 600,
+      poll: 2000
+    },
+    disableHostCheck: true,
+    sockHost: 'localhost',
+    sockPort: 8080
+  }
 }
